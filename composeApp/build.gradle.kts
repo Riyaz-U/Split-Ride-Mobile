a18057854fd0439d7d6f2496befade654f8ea8a3
Riyaz Uddin <riyazps007@gmail.com>
2026-02-08 19:49:00 +0530

    feat: added dependency injection module

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -39,6 +38,10 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
---
+++ composeApp/src/commonMain/kotlin/com/wiseowl/splitride/core/di/KoinModule.kt
@@ -0,0 +1,10 @@
+package com.wiseowl.splitride.core.di
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val sharedModule = module {
+
+}
+
+expect val platformModule: Module
