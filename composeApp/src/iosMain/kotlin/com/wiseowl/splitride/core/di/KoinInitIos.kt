cb7ed7b0ea5a37310b2df1086a61d169136d0499
Riyaz Uddin <riyazps007@gmail.com>
2026-02-12 00:33:43 +0530

    feat: added initialized for koin

---
+++ composeApp/src/iosMain/kotlin/com/wiseowl/splitride/core/di/KoinInitIos.kt
@@ -0,0 +1,22 @@
+package com.wiseowl.splitride.core.di
+
+fun initKoinIos() {
+    initKoin()
+}
+
+
+// iosApp/iOSApp.swift
+//import shared
+//
+//@main
+//struct iOSApp: App {
+//    init() {
+//        KoinInitIosKt.initKoinIos()
+//    }
+//
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
