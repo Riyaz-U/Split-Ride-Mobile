bd2ad510045565eab944b3755b5117bcd30f3aac
Riyaz Uddin <riyazps007@gmail.com>
2026-02-21 19:48:07 +0530

    api: added regex constraint

--- composeApp/src/commonMain/kotlin/com/wiseowl/splitride/authentication/domain/models/InputConstrain.kt
+++ composeApp/src/commonMain/kotlin/com/wiseowl/splitride/authentication/domain/models/InputConstrain.kt
@@ -4,4 +4,5 @@ sealed class Constraint {
     object NonEmpty : Constraint()
     data class MinLength(val length: Int) : Constraint()
     data class MaxLength(val length: Int) : Constraint()
+    class MatchRegex(val regex: String) : Constraint()
 }
