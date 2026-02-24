17b67cc942ce6a20ee7f2ed3f57e2de5e5ccd12f
Riyaz Uddin <riyazps007@gmail.com>
2026-02-24 23:12:41 +0530

    ui: animation

--- composeApp/src/commonMain/kotlin/com/wiseowl/splitride/onboarding/presentation/OnboardingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/wiseowl/splitride/onboarding/presentation/OnboardingScreen.kt
@@ -1,5 +1,8 @@
 package com.wiseowl.splitride.onboarding.presentation
 
+import androidx.compose.animation.core.AnimationState
+import androidx.compose.animation.core.animateTo
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -13,9 +16,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeGesturesPadding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,15 +23,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.wiseowl.splitride.core.ui.components.PrimaryButton
+import com.wiseowl.splitride.core.ui.models.ButtonState
 import com.wiseowl.splitride.core.ui.routing.EventBus
 import com.wiseowl.splitride.core.ui.routing.Navigation
 import com.wiseowl.splitride.core.ui.routing.Screen
@@ -62,55 +66,53 @@ fun OnboardingContent(
     onSkip: () -> Unit,
     onForward: () -> Unit,
 ) {
-    val pageState = rememberPagerState(initialPage = state.currentPage) { state.pages.size }
-    LaunchedEffect(state.currentPage) {
-        pageState.animateScrollToPage(state.currentPage)
-    }
     Box(
-        Modifier.fillMaxSize().background(Color(229, 231, 235)).padding(16.dp)
-    ){
+        Modifier.fillMaxSize().background(Color(229, 231, 235))
+    ) {
         Text(
             "Skip",
             fontWeight = FontWeight.Medium,
             color = Color(19, 91, 236),
             modifier = Modifier.align(Alignment.TopEnd).clickable { onSkip() }.safeGesturesPadding()
         )
-        HorizontalPager(
-            state = pageState
-        ) { pageNo ->
-            val page = state.pages[pageNo]
-            Box {
-                Column(
-                    Modifier.fillMaxSize().align(Alignment.Center),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(page.imageRes),
-                        contentDescription = null,
-                        modifier = Modifier.fillMaxWidth(0.8f).aspectRatio(1f)
-                    )
-                    Spacer(Modifier.height(32.dp))
-                    Text(
-                        page.title,
-                        fontSize = 30.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color(19, 91, 236)
-                    )
-                    Spacer(Modifier.height(16.dp))
-                    Text(
-                        page.description,
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.Center,
-                        lineHeight = 24.sp
-                    )
-                    Spacer(Modifier.height(32.dp))
-                    Button(
-                        onClick = onForward
-                    ) {
-                        Text(if (pageNo == state.pages.size - 1) "Get Started" else "Next")
-                    }
-                }
+        val page by rememberUpdatedState(state.pages[state.currentPage])
+        val animation = remember(page) { AnimationState(0f) }
+        LaunchedEffect(page){
+            animation.animateTo(1f, tween(700))
+        }
+        Box {
+            Column(
+                Modifier.fillMaxSize().align(Alignment.Center).alpha(animation.value),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(page.imageRes),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth(0.8f).aspectRatio(1f)
+                )
+                Spacer(Modifier.height(32.dp))
+                Text(
+                    page.title,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 38.sp,
+                    color = Color(19, 91, 236)
+                )
+                Spacer(Modifier.height(16.dp))
+                Text(
+                    page.description,
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 24.sp
+                )
+                Spacer(Modifier.height(32.dp))
+                PrimaryButton(
+                    modifier = Modifier.fillMaxWidth().padding(20.dp),
+                    button = ButtonState(text = if (state.currentPage == state.pages.size - 1) "Get Started" else "Next"),
+                    onClick = onForward
+                )
             }
         }
     }
