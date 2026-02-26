c42d196ee305a0d6da3c1cad68f3caebf2befcad
Riyaz Uddin <riyazps007@gmail.com>
2026-02-26 22:36:23 +0530

    ui: added span text

--- composeApp/src/commonMain/kotlin/com/wiseowl/splitride/core/ui/models/TextState.kt
+++ composeApp/src/commonMain/kotlin/com/wiseowl/splitride/core/ui/models/TextState.kt
@@ -1,9 +1,25 @@
 package com.wiseowl.splitride.core.ui.models
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import com.wiseowl.splitride.core.ui.routing.Intent
 
 data class TextState(
     val text: String,
     val color: Color = Color.Unspecified
 )
 
+data class SpanTextState(
+    val spans: List<TextSpan> = emptyList(),
+    val defaultColor: Color = Color.Unspecified
+)
+
+data class TextSpan(
+    val text: String,
+    val color: Color = Color.Unspecified,
+    val size: TextUnit = TextUnit.Unspecified,
+    val weight: FontWeight? = null,
+    val underline: Boolean = false,
+    val intent: Intent? = null
+)
