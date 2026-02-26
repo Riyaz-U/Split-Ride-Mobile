ca55f5424968ec3f039e2f698535b0788dc47c3d
Riyaz Uddin <riyazps007@gmail.com>
2026-02-26 09:28:07 +0530

    feat: incoorporated design components

--- composeApp/src/commonMain/kotlin/com/wiseowl/splitride/onboarding/presentation/models/OnboardingPage.kt
+++ composeApp/src/commonMain/kotlin/com/wiseowl/splitride/onboarding/presentation/models/OnboardingPage.kt
@@ -1,9 +1,10 @@
 package com.wiseowl.splitride.onboarding.presentation.models
 
+import com.wiseowl.splitride.core.ui.models.TextState
 import org.jetbrains.compose.resources.DrawableResource
 
 data class OnboardingPage(
-    val title: String,
-    val description: String,
+    val title: TextState,
+    val description: TextState,
     val imageRes: DrawableResource
 )
