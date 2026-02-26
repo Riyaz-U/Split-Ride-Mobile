107909fbe6ad15ccbc0feaf9157b5cd1573e1cd2
Riyaz Uddin <riyazps007@gmail.com>
2026-02-26 22:36:23 +0530

    ui: added span text

--- composeApp/src/commonMain/kotlin/com/wiseowl/splitride/core/ui/components/Texts.kt
+++ composeApp/src/commonMain/kotlin/com/wiseowl/splitride/core/ui/components/Texts.kt
@@ -8,13 +8,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.wiseowl.splitride.core.theme.AppColors
 import com.wiseowl.splitride.core.theme.interFontFamily
+import com.wiseowl.splitride.core.ui.models.SpanTextState
+import com.wiseowl.splitride.core.ui.models.TextSpan
 import com.wiseowl.splitride.core.ui.models.TextState
+import com.wiseowl.splitride.core.ui.routing.Navigation
+import com.wiseowl.splitride.core.ui.routing.Screen
 
 // 1. Headline Large — 36sp Bold
 @Composable
@@ -96,6 +105,61 @@ fun Caption(
     )
 }
 
+// 5. Caption — 14sp Regular
+@Composable
+fun SpanText(
+    modifier: Modifier = Modifier,
+    state: SpanTextState
+) {
+    Text(
+        text = buildAnnotatedString {
+            var spanStart = 0
+            state.spans.forEach { span ->
+                append(span.text)
+                val spanEnd = spanStart + span.text.length
+
+                val spanStyle = SpanStyle(
+                    color = span.color,
+                    fontSize = span.size,
+                    fontWeight = span.weight,
+                    textDecoration = if (span.underline) TextDecoration.Underline else TextDecoration.None
+                )
+
+                if (span.intent != null) {
+                    addLink(
+                        clickable = LinkAnnotation.Clickable(
+                            tag = span.intent.toString(),
+                            styles = TextLinkStyles(
+                                style = spanStyle,
+                                pressedStyle = spanStyle,
+                                hoveredStyle = spanStyle,
+                                focusedStyle = spanStyle
+                            )
+                        ) {
+                            //TODO: Handle click using some reducer holder
+                        },
+                        start = spanStart,
+                        end = spanEnd
+                    )
+                } else {
+                    addStyle(
+                        style = spanStyle,
+                        start = spanStart,
+                        end = spanEnd
+                    )
+                }
+
+                spanStart += span.text.length
+            }
+        },
+        modifier = modifier,
+        fontFamily = interFontFamily(),
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        color = state.defaultColor
+    )
+}
+
 // region Previews
 
 @Preview
@@ -110,6 +174,42 @@ private fun TypographyPreview() {
         Subheading(state = TextState(text = "Subheading", color = AppColors.TextPrimary))
         Body(state = TextState(text = "Body text", color = AppColors.TextSecondary))
         Caption(state = TextState(text = "Caption text", color = AppColors.TextMuted))
+        SpanText(
+            state = SpanTextState(
+                spans = listOf(
+                    TextSpan(
+                        text = "This ",
+                        color = Color.Red,
+                        size = 20.sp,
+                        weight = FontWeight.Bold
+                    ),
+                    TextSpan(
+                        text = "is ",
+                        color = Color.Green,
+                        size = 20.sp,
+                        weight = FontWeight.Bold
+                    ),
+                    TextSpan(
+                        text = "Spannable ",
+                        color = Color.Blue,
+                        size = 20.sp,
+                        weight = FontWeight.Bold
+                    ),
+                    TextSpan(
+                        text = "Click here",
+                        color = AppColors.Primary,
+                        weight = FontWeight.Bold,
+                        underline = false,
+                        intent = Navigation(Screen.Home)
+                    ),
+                    TextSpan(
+                        text = " for more info.",
+                        color = AppColors.TextSecondary
+                    )
+                ),
+                defaultColor = AppColors.TextMuted
+            )
+        )
     }
 }
 
