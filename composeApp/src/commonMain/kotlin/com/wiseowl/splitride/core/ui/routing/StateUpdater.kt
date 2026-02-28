9e9da3bbdff1418d423a3c9c3e5cc6c579207c75
Riyaz Uddin <riyazps007@gmail.com>
2026-02-28 22:30:21 +0530

    api: reducer to have stateupdater as param

--- composeApp/src/commonMain/kotlin/com/wiseowl/splitride/core/ui/routing/StateUpdater.kt
+++ composeApp/src/commonMain/kotlin/com/wiseowl/splitride/core/ui/routing/StateUpdater.kt
@@ -9,7 +9,7 @@ class StateUpdater(
     fun processIntent(intent: Intent) {
         val reducerHolder = reducers[intent::class.qualifiedName]
         if (reducerHolder != null) {
-            reducerHolder.reducer.invoke(intent)
+            reducerHolder.reducer.invoke(intent, this)
         } else {
             // EventBus.push is suspend; if you want fire-and-forget, decide where the coroutine scope lives.
             // Keeping existing behavior would require calling this from a coroutine.
@@ -24,13 +24,13 @@ class StateUpdater(
 class ReducerBuilder{
     val reducers = mutableMapOf<String, ReducerHolder<Intent>>()
 
-    inline fun <reified I: Intent> on(noinline reducer: I.() -> Unit){
-        reducers.put(I::class.qualifiedName!!, ReducerHolder(reducer as Intent.() -> Unit))
+    inline fun <reified I: Intent> on(noinline reducer: I.(StateUpdater) -> Unit){
+        reducers.put(I::class.qualifiedName!!, ReducerHolder(reducer as Intent.(StateUpdater) -> Unit))
     }
 
     fun build(): MutableMap<String, ReducerHolder<Intent>> = reducers
 }
 
 class ReducerHolder<I: Intent>(
-    val reducer: I.() -> Unit
+    val reducer: I.(StateUpdater) -> Unit
 )
