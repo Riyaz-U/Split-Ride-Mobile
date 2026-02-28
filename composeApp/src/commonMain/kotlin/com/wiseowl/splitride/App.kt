aecf1fb66edd4c7615e657af83d9578256bed9d2
Riyaz Uddin <riyazps007@gmail.com>
2026-02-28 23:16:25 +0530

    ui: implemented snackbar

--- composeApp/src/commonMain/kotlin/com/wiseowl/splitride/App.kt
+++ composeApp/src/commonMain/kotlin/com/wiseowl/splitride/App.kt
@@ -2,17 +2,25 @@ package com.wiseowl.splitride
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.*
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
-import com.wiseowl.splitride.core.theme.LightColors
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.wiseowl.splitride.core.storage.UserDetailStorage
+import com.wiseowl.splitride.core.theme.AppColors
+import com.wiseowl.splitride.core.theme.LightColors
 import com.wiseowl.splitride.core.ui.routing.CompletedOnboarding
 import com.wiseowl.splitride.core.ui.routing.EventBus
 import com.wiseowl.splitride.core.ui.routing.Navigation
 import com.wiseowl.splitride.core.ui.routing.Root
 import com.wiseowl.splitride.core.ui.routing.Screen
+import com.wiseowl.splitride.core.ui.routing.SnackBar
 import org.koin.compose.koinInject
 
 @Composable
@@ -21,9 +29,19 @@ fun App() {
     MaterialTheme(
         colorScheme = LightColors
     ) {
-        Scaffold {
+        val snackBarHostState = remember { SnackbarHostState() }
+        Scaffold(
+            snackbarHost = {
+                SnackbarHost(snackBarHostState){
+                    Snackbar(
+                        it,
+                        containerColor = AppColors.SecondaryContainer
+                    )
+                }
+            }
+        ) {
             val navController = rememberNavController()
-            EventListener(navController)
+            EventListener(navController, snackBarHostState)
             Root(navController)
         }
     }
@@ -31,7 +49,8 @@ fun App() {
 
 @Composable
 fun EventListener(
-    navHostController: NavHostController
+    navHostController: NavHostController,
+    snackBarHostState: SnackbarHostState,
 ) {
     val eventBus = koinInject<EventBus>()
     val userDetailStorage = koinInject<UserDetailStorage>()
@@ -39,12 +58,20 @@ fun EventListener(
     LaunchedEffect(eventBus) {
         val channel = eventBus.subscribe()
         for (event in channel) {
-            when(event){
-               is Navigation -> navHostController.navigate(event.screen)
-               is CompletedOnboarding -> {
-                   userDetailStorage.markOnboardingCompleted()
-                   navHostController.navigate(Screen.Home)
-               }
+            when (event) {
+                is Navigation -> navHostController.navigate(event.screen)
+                is CompletedOnboarding -> {
+                    userDetailStorage.markOnboardingCompleted()
+                    navHostController.navigate(Screen.Home)
+                }
+                is SnackBar -> {
+                    snackBarHostState.currentSnackbarData?.dismiss()
+                    val result = snackBarHostState.showSnackbar(message = event.text, actionLabel = event.actionLabel)
+                    when(result){
+                        SnackbarResult.Dismissed -> Unit
+                        SnackbarResult.ActionPerformed -> event.action?.let { event.stateUpdater?.processIntent(it) }
+                    }
+                }
             }
         }
     }
